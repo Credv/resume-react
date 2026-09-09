5cc565fd777dbd3d8cf1afce95fccd127ab0ab4f
陈鸿 <ch01390250@antgroup.com>
2026-09-09 14:46:52 +0800

    fix: 在 index.html head 中立即执行 localStorage 设置，确保 Netlify 角标隐藏生效

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// 隐藏 Netlify 角标
-if (typeof window !== 'undefined') {
-  localStorage.setItem('nl-hud:public:v1', 'hidden')
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
